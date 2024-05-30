af0f2322a5e8e92e0032b20e9c7dca60f57b799e
iamheaven <125787194+iamaworld@users.noreply.github.com>
2024-05-30 19:27:14 +0500

    now

--- app/api/api-hooks.js
+++ app/api/api-hooks.js
@@ -2,15 +2,19 @@ import { useEffect, useState } from "react";
 import { getNormalizedGamesDataByCategory, isResponseOk } from "./api-utils";
 
 export const useGetDataByCategory = (endpoint, category) => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getNormalizedGamesDataByCategory(endpoint, category);
-      setData(data);
+  try {
+    const [data, setData] = useState(null);
+    useEffect(() => {
+      async function fetchData() {
+        const data = await getNormalizedGamesDataByCategory(endpoint, category);
+        setData(data);
+      }
+      fetchData();
+    }, []);
+    if (isResponseOk) {
+      return data;
     }
-    fetchData();
-  }, []);
-  if (isResponseOk) {
-    return data;
+  } catch (error) {
+    return error;
   }
 };
