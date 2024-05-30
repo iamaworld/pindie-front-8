a88fc3048ace1219b8ef79775c7e9ad675e511c8
iamheaven <125787194+iamaworld@users.noreply.github.com>
2024-05-30 20:05:29 +0500

    now

--- app/games/[id]/page.js
+++ app/games/[id]/page.js
@@ -24,7 +24,7 @@ export default function GamePage(props) {
       setPreloaderVisible(true);
       const game = await getNormalizedGameDataById(
         endpoints.games,
-        props.params.id
+        props.params.link
       );
       isResponseOk(game) ? setGame(game) : setGame(null);
       setPreloaderVisible(false);
@@ -45,11 +45,7 @@ export default function GamePage(props) {
       ? game.users.map((user) => user.id)
       : [];
     usersIdArray.push(authContext.user.id); // Данные о пользователе получаем из контекста
-    const response = await vote(
-      `${endpoints.games}/${game.id}`,
-      jwt,
-      usersIdArray
-    );
+    const response = await vote(`${game.link}`, jwt, usersIdArray);
     if (isResponseOk(response)) {
       setGame(() => {
         return {
