49350a07b305595c086a47d11a0cc0d615b906ff
iamheaven <125787194+iamaworld@users.noreply.github.com>
2024-05-30 19:06:40 +0500

    now

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://pindiebackend.nomorepartiesco.r/api";
+export const BASE_URL = "https://pindiebackend.nomorepartiesco.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
